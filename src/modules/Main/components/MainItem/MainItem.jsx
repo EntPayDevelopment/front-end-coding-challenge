d932f49c93bdff75fcec21d9da07c66895dde525
addiescode-sj <addiescode@gmail.com>
2022-05-12 20:36:22 -0400

    feat: add case for errors

--- src/modules/Main/components/MainItem/MainItem.jsx
+++ src/modules/Main/components/MainItem/MainItem.jsx
@@ -4,35 +4,40 @@ import { addToWatchlist, removeFromWatchlist } from "@api";
 
 function MainItem(props) {
   const { imgUrl, title, id } = props;
-
-  const [isAdded, setIsAdded] = useState(true);
+  const [isAdded, setIsAdded] = useState(false);
 
   const handleAddCard = async () => {
     await addToWatchlist(id)
       .then(() => {
         setIsAdded(true);
-        alert(`${id} is added!`);
       })
-      .catch(console.error);
+      .catch((error) => {
+        if (error.response.status === 422) {
+          alert(`Error: ${error.message}. Please try again!`);
+        }
+      });
   };
 
   const handleRemoveCard = async () => {
     await removeFromWatchlist(id)
       .then(() => {
         setIsAdded(false);
-        alert(`${id} is removed!`);
       })
       .catch(console.error);
   };
 
   return (
     <div className="main-item-card">
-      <img src={imgUrl} alt="main-card-image" />
-      <title>{title}</title>
-      {isAdded && (
-        <button onClick={handleRemoveCard}>Remove from watchlist</button>
-      )}
-      {!isAdded && <button onClick={handleAddCard}>Add to watchlist</button>}
+      <div className="main-item-content">
+        <div className="card-image-wrapper">
+          <img src={imgUrl} alt="main-card-image" />
+        </div>
+        <p>{title}</p>
+        {isAdded && (
+          <button onClick={handleRemoveCard}>Remove from watchlist</button>
+        )}
+        {!isAdded && <button onClick={handleAddCard}>Add to watchlist</button>}
+      </div>
     </div>
   );
 }
